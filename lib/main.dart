2b7c11263a14a9aff3717383cf0d891d282958cc
hwangblood <hwangblood@gmail.com>
2023-01-28 01:08:37 +0800

    MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'models/data.dart' as data;
+import 'models/models.dart';
+import 'widgets/email_list_view.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+      theme: ThemeData.light(useMaterial3: true),
+      home: Feed(currentUser: data.user_0),
+    );
+  }
+}
+
+class Feed extends StatefulWidget {
+  const Feed({
+    super.key,
+    required this.currentUser,
+  });
+
+  final User currentUser;
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  late final _colorScheme = Theme.of(context).colorScheme;
+  late final _backgroundColor = Color.alphaBlend(
+      _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: _backgroundColor,
+        child: EmailListView(
+          currentUser: widget.currentUser,
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _colorScheme.tertiaryContainer,
+        foregroundColor: _colorScheme.onTertiaryContainer,
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
